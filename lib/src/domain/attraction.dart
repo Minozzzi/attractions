83bf37860e43cf7bdc83be66b701061d1085b7a5
Guilherme Minozzi <minozzi60@gmail.com>
2023-03-30 10:04:18 -0300

    feat(domain): add attraction model

---
+++ lib/src/domain/attraction.dart
@@ -0,0 +1,39 @@
+class Attraction {
+  static const FIELD_ID = 'id';
+  static const FIELD_NAME = 'name';
+  static const FIELD_DESCRIPTION = 'description';
+  static const FIELD_DIFFERENTIALS = 'differentials';
+  static const FIELD_CREATED_AT = 'createdAt';
+
+  int id;
+  final String name;
+  final String description;
+  final String? differentials;
+  final DateTime createdAt;
+
+  Attraction(
+      {required this.id,
+      required this.name,
+      required this.description,
+      this.differentials,
+      required this.createdAt});
+
+  factory Attraction.fromJson(Map<String, dynamic> json) {
+    return Attraction(
+        id: json['id'],
+        name: json['name'],
+        description: json['description'],
+        differentials: json['differentials'],
+        createdAt: json['createdAt']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'differentials': differentials,
+      'createdAt': createdAt
+    };
+  }
+}
