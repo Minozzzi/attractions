d6ed4b7a7bab76df9f81a6ebb6e1a191c5d9cb75
Guilherme Minozzi <minozzi60@gmail.com>
2023-04-23 21:17:15 -0300

    refactor: use database on attraction repository

--- lib/src/data/attraction_repository.dart
+++ lib/src/data/attraction_repository.dart
@@ -1,25 +1,59 @@
+import 'package:attractions/src/database/db_provider.dart';
 import 'package:attractions/src/domain/attraction.dart';
+import 'package:sqflite/sqflite.dart';
 
 class AttractionRepository {
-  final List<Attraction> _attractions = [];
+  final dbProvider = DatabaseProvider.instance;
 
-  Future<List<Attraction>> getAttractions() async {
-    return _attractions;
+  late Database database;
+
+  AttractionRepository() {
+    _init();
+  }
+
+  Future<void> _init() async {
+    database = await dbProvider.database;
+  }
+
+  Future<List<Attraction>> getAttractions({
+    String filters = '',
+    String fieldOrderBy  = Attraction.FIELD_ID,
+    bool ascending = true,
+  }) async {
+    String? where;
+
+    if (filters.isNotEmpty) {
+      where = "UPPER(${Attraction.FIELD_NAME}) LIKE UPPER('%$filters%')";
+    }
+
+    final orderBy = '$fieldOrderBy ${ascending ? 'ASC' : 'DESC'}';
+
+    final attractions = await database.query(
+      Attraction.TABLE_NAME,
+      where: where,
+      orderBy: orderBy,
+    );
+
+    return attractions.map((attraction) => Attraction.fromJson(attraction)).toList();
+
   }
 
   Future<void> addAttraction(Attraction attraction) async {
-    _attractions.add(attraction);
+    final values = attraction.toJson();
+    values.remove(Attraction.FIELD_ID);
+    final id = await database.insert(Attraction.TABLE_NAME, values);
+    attraction.id = id;
   }
 
   Future<void> updateAttraction(Attraction attraction) async {
-    final attractionIndexToUpdate =
-        _attractions.indexWhere((item) => item.id == attraction.id);
-    if (attractionIndexToUpdate == -1) return;
-
-    _attractions[attractionIndexToUpdate] = attraction;
+    final values = attraction.toJson();
+    values.remove(Attraction.FIELD_ID);
+    await database.update(Attraction.TABLE_NAME, values,
+        where: '${Attraction.FIELD_ID} = ?', whereArgs: [attraction.id]);
   }
 
   Future<void> removeAttraction(Attraction attraction) async {
-    _attractions.remove(attraction);
+    await database.delete(Attraction.TABLE_NAME,
+        where: '${Attraction.FIELD_ID} = ?', whereArgs: [attraction.id]);
   }
 }
